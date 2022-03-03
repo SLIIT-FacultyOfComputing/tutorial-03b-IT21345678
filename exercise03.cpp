3ddace8d7b99e66c137902c599ebf48258be5f58
IT21345678 <99394842+IT21345678@users.noreply.github.com>
2022-03-03 07:17:32 +0000

    Did Totorial 3

--- exercise03.cpp
+++ exercise03.cpp
@@ -4,40 +4,55 @@ using namespace std;
 
 int volume(int height, int width, int length);
 
-// 1. Define a structure called Box
+struct box{
+	
+	int length;
+	int width;
+	int height;
+
+	
+};// 1. Define a structure called Box
 // have the integer data types Height, Width, Length
 
 // Do not change the main function
 int main() {
-    // 2. Create a variable called box1 of the Box structure type
+   box box1;
+  box box2;// 2. Create a variable called box1 of the Box structure type
     // int box1Height, box1Width, box1Length;
     // 3. Create a variable called box2 of the Box structure type
     
     // int box2Height, box2Width, box2Length;
-    int totalVolume;
+    int TotalVolume;
     
     // 4. Input the height, width, lenght of box1 and box2
-    cout << "Enter Box 1 Height : ";
-    cin >> Height of Box 1;
-    cout << "Enter Box 1 Width : ";
-    cin >> Width of Box 1;
-    cout << "Enter Box 1 Length : ";
-    cin >> Length of Box 1;
-    
-    cout << "Enter Box 2 Height : ";
-    cin >> Height of Box 2;
-    cout << "Enter Box 2 Width : ";
-    cin >> Width of Box 2;
-    cout << "Enter Box 2 Length : ";
-    cin >> Length of Box 2;
+   cout << "Enter Box 1 Height : ";
+cin >> box1.length;
+	
+cout << "Enter Box 1 width : ";
+cin >> box1.width;
+
+cout << "Enter Box 1 length : ";
+cin >> box1.height;
+
+cout << "Enter Box 2 Height : ";
+cin >> box2.length;
+
+cout << "Enter Box 2 width : ";
+cin >> box2.width;
+
+cout << "Enter Box 2 length : ";
+cin >> box2.height;
     
     // 5. Replace the coding below to pass the Box type structure
-    totalVolume = volume( , , )
-             + volume( , , );
+    	TotalVolume = volume(box1.height,box1.width,box1.length)+volume(box2.height,box2.width,box2.length);
     
-    cout << "Volume of Box is " << totalVolume << endl;
+    cout << "Volume of Box is " << TotalVolume << endl;
     
     return 0;
 }
 
-// Implement the functions here
+int volume(int height,int width,int length){
+	
+	return length*height*width;
+	
+}
